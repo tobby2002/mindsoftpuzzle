b1498f42e4aabf94f62780d47e4b9e3a22cc449e
neo1 <tobby2002@gmailcom>
2019-03-28 21:49:11 +0900

    first

---
+++ keystone.js
@@ -0,0 +1,97 @@
+// Load the babel-register plugin for the graphql directory
+// Note this checks the regex against an absoloute path
+require('babel-register')({ only: /\/graphql\/.*/ });
+
+// Load .env config for development environments
+require('dotenv').config({ silent: true });
+
+// Initialise New Relic if an app name and license key exists
+if (process.env.NEW_RELIC_APP_NAME && process.env.NEW_RELIC_LICENSE_KEY) {
+	require('newrelic');
+}
+
+/**
+ * Application Initialisation
+ */
+
+var keystone = require('keystone');
+var pkg = require('./package.json');
+
+keystone.init({
+
+	'name': 'mindmap',
+	'brand': 'MINDMAP AI',
+	'back': '/me',
+
+	'favicon': 'public/favicon.ico',
+	'less': 'public',
+	'static': 'public',
+
+	'views': 'templates/views',
+	'view engine': 'jade',
+	'view cache': false,
+
+	'emails': 'templates/emails',
+
+	'auto update': true,
+	'mongo': process.env.MONGO_URI || 'mongodb://neo:neo123@localhost/' + pkg.name,
+	// 'mongo': process.env.MONGO_URI || 'mongodb://localhost/' + pkg.name,
+
+	'session': true,
+	'session store': 'mongo',
+	'auth': true,
+	'user model': 'User',
+	'cookie secret': process.env.COOKIE_SECRET || 'mrmind',
+
+	'mandrill api key': process.env.MANDRILL_KEY,
+
+	'google api key': process.env.GOOGLE_BROWSER_KEY,
+	'google server api key': process.env.GOOGLE_SERVER_KEY,
+
+	'ga property': process.env.GA_PROPERTY,
+	'ga domain': process.env.GA_DOMAIN,
+
+	'chartbeat property': process.env.CHARTBEAT_PROPERTY,
+	'chartbeat domain': process.env.CHARTBEAT_DOMAIN,
+
+	'basedir': __dirname
+
+});
+
+keystone.import('models');
+
+keystone.set('routes', require('./routes'));
+
+keystone.set('locals', {
+	_: require('lodash'),
+	moment: require('moment'),
+	js: 'javascript:;',
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	plural: keystone.utils.plural,
+	editable: keystone.content.editable,
+	google_api_key: keystone.get('google api key'),
+	ga_property: keystone.get('ga property'),
+	ga_domain: keystone.get('ga domain'),
+	chartbeat_property: keystone.get('chartbeat property'),
+	chartbeat_domain: keystone.get('chartbeat domain')
+});
+
+keystone.set('email locals', {
+	utils: keystone.utils,
+	host: (function() {
+		if (keystone.get('env') === 'staging') return 'http://sydjs-beta.herokuapp.com';
+		if (keystone.get('env') === 'production') return 'http://mindmap.ai';
+		return (keystone.get('host') || 'http://localhost:') + (keystone.get('port') || '80');
+		// return (keystone.get('host') || 'http://45.76.103.214:') + (keystone.get('port') || '80');
+	})()
+});
+
+keystone.set('nav', {
+	'meetups': ['meetups', 'talks', 'rsvps'],
+	'members': ['users', 'organisations'],
+	'posts': ['posts', 'post-categories', 'post-comments'],
+	'links': ['links', 'link-tags', 'link-comments']
+});
+
+keystone.start();
